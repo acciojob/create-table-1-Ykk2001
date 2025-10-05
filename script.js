4921d453f2264b5a85408a4f87882f27fea68c14
Ykk2001 <160621076+Ykk2001@users.noreply.github.com>
2025-10-05 12:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let sampleTable = document.getElementById("sampleTable");
 function insert_Row() {
-    //Write your code here
-  
-  
-}
+        //creation of the new row
+        let newRow = document.createElement("tr");
+        //created two td ele
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        //added inner text into them
+        td1.innerText = "New Cell1";
+        td2.innerText = "New Cell2";
+        //inserted that td inside the newRow
+        newRow.appendChild(td1);
+        newRow.appendChild(td2);
+        //new row inserted before the first child
+        sampleTable.insertBefore(newRow, sampleTable.firstChild);
+      }
